0c6fe40726a065361650790d9500cdaea3ca2ace
Artur <art.oganyan04@gmail.com>
2023-05-19 12:57:10 +0200

    Initial commit

---
+++ NeuralNetwork.py
@@ -0,0 +1,18 @@
+class NeuralNetwork:
+    def __init__(self, propagation_method, activation_function, num_of_layers, num_of_nodes):
+        self.propagation_method=propagation_method
+        self.activation_function=activation_function
+        self.num_of_layers=num_of_layers
+        self.num_of_nodes=num_of_nodes
+
+    def fit(self):
+        return 0
+    
+    def predict(self):
+        return 0
+
+    def predict_prob(self):
+        return 0
+
+    def score(self):
+        return 0
